13ab35b89cb0af1e221dcf0889bc8a47284211c5
abelaethio <114033068+abelaethio@users.noreply.github.com>
2023-06-28 23:49:38 -0700

    Update script.js

--- Week 1/script.js
+++ Week 1/script.js
@@ -1,1 +1,9 @@
+ let firstName = "Abel";
+    let lastName = "Melese";
+    let yearsOfStudy = "5 months";
+    let goal = "work in the IT";
 
+    let result = "My name is " + firstName + " " + lastName + ".<br>";
+    result += "I have been at SCC for " + yearsOfStudy + " and my plan is to " + goal + " after completing my courses.";
+
+     document.getElementById("result").innerHTML = result;
